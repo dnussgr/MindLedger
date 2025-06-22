011d4778e7ddf7d64b99bbeae34e3cd9c9c4c7ae
Dominic <dominic.nussgraber@gmail.com>
2025-06-22 09:32:34 +0200

    Add rudimentary text editing toolbar

--- MindLedger.App/MainWindow.xaml.cs
+++ MindLedger.App/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Windows;
-using MindLedger.AppLogic.ViewModels;
+﻿using MindLedger.AppLogic.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Forms;
 
 namespace MindLedger.App;
 
@@ -10,4 +14,24 @@ public partial class MainWindow : Window
         InitializeComponent();
         DataContext = viewModel;
     }
+
+    private void FontSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (e.AddedItems[0] is ComboBoxItem item && double.TryParse(item.Content.ToString(), out double size))
+        {
+            var range = richTextBox.Selection;
+            range.ApplyPropertyValue(TextElement.FontSizeProperty, size);
+        }
+    }
+
+    private void ChangeTextColor_Click(object sender, RoutedEventArgs e)
+    {
+        var dialog = new System.Windows.Forms.ColorDialog();
+        if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+        {
+            var wpfColor = Color.FromArgb(dialog.Color.A, dialog.Color.R, dialog.Color.G, dialog.Color.B);
+            richTextBox.Selection.ApplyPropertyValue(TextElement.ForegroundProperty, new SolidColorBrush(wpfColor));
+        }
+    }
+
 }
